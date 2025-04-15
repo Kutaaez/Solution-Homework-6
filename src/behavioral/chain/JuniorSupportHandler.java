b6ce8d7d7ec6df09715002b7707b9169ea49de07
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-15 21:59:40 +0300

    add uniorupportandler to handle billing and refund requests

---
+++ src/behavioral/chain/JuniorSupportHandler.java
@@ -0,0 +1,12 @@
+package behavioral.chain;
+
+public class JuniorSupportHandler extends SupportHandler {
+    @Override
+    public void handle(String issue) {
+        if (issue.equals("refund_request") || issue.equals("billing_issue")) {
+            System.out.println("[JuniorSupport] Handled " + issue);
+        } else if (next != null) {
+            next.handle(issue);
+        }
+    }
+}
