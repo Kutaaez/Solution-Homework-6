53259cd0183a42274b991ff62b8013198640f73b
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-15 21:59:07 +0300

    implement otandler for password_reset issues

---
+++ src/behavioral/chain/FAQBotHandler.java
@@ -0,0 +1,12 @@
+package behavioral.chain;
+
+public class FAQBotHandler extends SupportHandler {
+    @Override
+    public void handle(String issue) {
+        if (issue.equals("password_reset")) {
+            System.out.println("[FAQBot] Handled " + issue);
+        } else if (next != null) {
+            next.handle(issue);
+        }
+    }
+}
