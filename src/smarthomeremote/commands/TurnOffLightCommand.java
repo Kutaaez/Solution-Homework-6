1312766d9ccf0a7de140df0281be3fec35a6b5a5
Miramuly Zharaskhan <66755936+Kutaaez@users.noreply.github.com>
2025-04-15 22:12:19 +0300

    add turn on/of command

---
+++ src/smarthomeremote/commands/TurnOffLightCommand.java
@@ -0,0 +1,21 @@
+package smarthomeremote.commands;
+
+import smarthomeremote.devices.Light;
+
+public class TurnOffLightCommand implements Command {
+    private Light light;
+
+    public TurnOffLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOn();
+    }
+}
